b41bf38bf161c6e50a6a0adac9ce77d2d4d50187
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-05-29 21:25:51 -0500

    fix(all): Enable JPA Auditing

--- src/main/java/pe/upc/learningcenter/LearningCenterApplication.java
+++ src/main/java/pe/upc/learningcenter/LearningCenterApplication.java
@@ -2,8 +2,10 @@ package pe.upc.learningcenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LearningCenterApplication {
 
 	public static void main(String[] args) {
